be5d86f03fb8463f41e78473499a5f7b7ba28fb3
JulienTouchard <julien.touchard@free.fr>
2023-11-06 11:45:22 +0100

    add css for textes

--- assets/js/modules/slider.js
+++ assets/js/modules/slider.js
@@ -14,6 +14,27 @@ const initSlider = () => {
     imgA.src = coverUrl + catalogue[currentImg].cover;
     imgA.id = "imgA";
     sliderHTML.append(imgA);
+    // creation d'un premier element text
+    const textesA = document.createElement("div");
+    textesA.id = "textesA";
+    sliderHTML.append(textesA);
+    const pA1 = document.createElement("p");
+    pA1.innerText = catalogue[currentImg].titre;
+    textesA.append(pA1);
+    const pA2 = document.createElement("p");
+    pA2.innerText = catalogue[currentImg].artiste;
+    textesA.append(pA2);
+    //
+    const textesB = document.createElement("div");
+    textesB.id = "textesB";
+    sliderHTML.append(textesB);
+    const pB1 = document.createElement("p");
+    pB1.innerText = catalogue[currentImg].titre;
+    textesB.append(pB1);
+    const pB2 = document.createElement("p");
+    pB2.innerText = catalogue[currentImg].artiste;
+    textesB.append(pB2);
+
 }
 const nextSlider=()=>{
     if (currentImg < catalogue.length - 1) {
@@ -22,10 +43,17 @@ const nextSlider=()=>{
         currentImg = 0;
     }
     document.querySelector("#coverSlider").src = coverUrl + catalogue[currentImg].cover;
+    document.querySelector("#textesB p:first-child").innerText = catalogue[currentImg].titre;
+    document.querySelector("#textesB p:last-child").innerText = catalogue[currentImg].artiste;
     document.querySelector("#imgA").classList.add("transSlider","slideRight");
+    document.querySelector("#textesA").classList.add("transSlider","slideRight");
     setTimeout(()=>{
         document.querySelector("#imgA").src = coverUrl + catalogue[currentImg].cover;
+        document.querySelector("#textesA p:first-child").innerText = catalogue[currentImg].titre;
+        document.querySelector("#textesA p:last-child").innerText = catalogue[currentImg].artiste;
         document.querySelector("#imgA").classList.remove("transSlider","slideRight");
+        document.querySelector("#textesA").classList.remove("transSlider","slideRight");
+
     },500)
     
 }
